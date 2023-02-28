f7a5aa2e1fd7cf8f51e40f094b4ff235e2755eea
Pedro Lorandi <hello@pedrolorandi.com>
2023-02-28 08:41:58 -0500

    Add button functions and styles

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,14 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let className = classNames(
+      'button', {
+      'button--confirm': props.confirm,
+      'button--danger': props.danger
+   })
+
+   return <button disabled={props.disabled} onClick={props.onClick} className={className}>{props.children}</button>;
 }
